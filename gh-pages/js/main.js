c983f8b322ab6809b6a66b1ae3d144603e8c7fe2
Kasper Koman <kasper.koman@gmail.com>
2017-05-06 11:47:38 +0200

    Added toggle and reset button

--- gh-pages/js/main.js
+++ gh-pages/js/main.js
@@ -6,4 +6,18 @@ $(function() {
 	$('button.hamburger').on('click', function() {
 		$(this).toggleClass('active');
 	});
+
+	$('#reset').on('click', function(e) {
+		e.preventDefault();
+		$('button.hamburger').each(function() {
+			$(this).removeClass('active');
+		});
+	})
+
+	$('#toggle-all').on('click', function(e) {
+		e.preventDefault();
+		$('button.hamburger').each(function() {
+			$(this).toggleClass('active');
+		});
+	})
 });
