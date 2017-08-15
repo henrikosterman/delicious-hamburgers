c412ebce7923c68f577b94c7193df0e9ac29bc0a
Kasper Koman <kasper.koman@gmail.com>
2017-08-15 18:23:35 +0200

    Set up jekyll, file separation, load bootstrap & font-awesome

--- gulpfile.js
+++ gulpfile.js
@@ -38,37 +38,3 @@ gulp.task('build', function(callback) {
 });
 
 gulp.task('default', ['build']);
-
-// GitHub Pages
-gulp.task('gh-pages', function(callback) {
-    gulpSequence('gh-pages-build')(callback);
-    gulp.watch('gh-pages/**/*.scss', ['gh-pages-css']);
-    gulp.watch('gh-pages/**/*.js', ['gh-pages-js']);
-});
-
-gulp.task('gh-pages-build', function(callback) {
-    gulpSequence('clean', ['gh-pages-css', 'gh-pages-js', 'gh-pages-fonts'])(callback);
-});
-
-gulp.task('gh-pages-css', function() {
-    return gulp.src('gh-pages/scss/main.scss')
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(autoprefixer('last 5 versions'))
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('dist/css'));
-});
-
-gulp.task('gh-pages-fonts', function() {
-    return gulp.src(config.bowerDir + '/components-font-awesome/fonts/**.*')
-        .pipe(gulp.dest('dist/fonts'));
-});
-
-gulp.task('gh-pages-js', function() {
-    return gulp.src([
-            config.bowerDir + '/jquery/dist/jquery.js', 
-            'gh-pages/**/*.js'
-        ])
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
